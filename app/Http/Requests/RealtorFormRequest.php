90d392ca75c72139d53c0c7ba07892e7b44d37ad
joaogabriei <joaonalves1@gmail.com>
2024-06-23 13:17:30 -0300

    validação de cpf e creci já existentes

--- app/Http/Requests/RealtorFormRequest.php
+++ app/Http/Requests/RealtorFormRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class RealtorFormRequest extends FormRequest
 {
@@ -22,8 +23,8 @@ class RealtorFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'cpf' => ['required', 'digits:11', 'numeric'],
-            'creci' => ['required', 'min:2', 'numeric'],
+            'cpf' => ['required', 'digits:11', 'numeric', 'unique:realtors,cpf'],
+            'creci' => ['required', 'min:2', 'numeric', 'unique:realtors,creci'],
             'name' => ['required', 'min:3'],
         ];
     }
@@ -33,11 +34,13 @@ class RealtorFormRequest extends FormRequest
         return [
             'cpf.required' => 'O campo CPF é obrigatório.',
             'cpf.digits' => 'O campo CPF precisa ter 11 caracteres.',
-            'cpf.numeric' => 'O campo CPF precisa ser um número.', 
+            'cpf.numeric' => 'O campo CPF precisa ser um número.',
+            'cpf.unique' => 'O CPF digitado já está sendo utilizado.', 
 
             'creci.required' => 'O campo CRECI é obrigatório.',
             'creci.min' => 'O campo CRECI precisa ter pelo menos :min caracteres',
             'creci.numeric' => 'O campo CRECI precisa ser um número.',
+            'creci.unique' => 'O CRECI digitado já está sendo utilizado.', 
 
             'name.required' => 'O campo nome é obrigatório.',
             'name.min' => 'O campo nome precisa ter pelo menos :min caracteres'
