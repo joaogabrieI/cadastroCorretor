a222d257f21529fb0bb306ff80fa07a569916f72
joaogabriei <joaonalves1@gmail.com>
2024-06-23 11:39:06 -0300

    migration de corretores

---
+++ database/migrations/2024_06_23_142628_create_realtors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('realtors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->integer('cpf');
+            $table->integer('creci');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('realtors');
+    }
+};
