ec5c382803c065f61d8a15e887da88423850863c
joaogabriei <joaonalves1@gmail.com>
2024-06-23 15:52:16 -0300

    estilização página inicial e edição

--- resources/views/realtors/edit.blade.php
+++ resources/views/realtors/edit.blade.php
@@ -4,38 +4,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Editar Corretor</title>
 </head>
 
 <body>
-    <main>
+    <main class="w-50 mt-3 container d-flex flex-column justify-content-center align-itens-center">
         <section>
-            <a href="{{route('realtors.index')}}">Voltar</a>
-            <h1>Editar dados do Corretor</h1>
-            @isset($sucessMessage)
-            <div>
-                {{$sucessMessage}}
-            </div>
-            @endisset
-            <form action="{{route('realtors.update', $realtor->id)}}" method="post">
+            <h1 class="text-center mb-3">Editar dados do corretor</h1>
+            <form action="{{route('realtors.update', $realtor->id)}}" method="post" class="row g-3">
                 @csrf
                 @method('put')
-                <input type="text" name="cpf" placeholder="Digite seu CPF" value="{{old('cpf')}}" value="{{$realtor->cpf}}">
-                @error('cpf')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+                <div class="mb-3 col-md-3">
+                    <input type="text" name="cpf" placeholder="Digite seu CPF" value="{{$realtor->cpf}}" autofocus class="form-control">
+                    @error('cpf')
+                    <div class="mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <input type="text" name="creci" placeholder="Digite seu Creci" value="{{old('creci')}}" value="{{$realtor->creci}}">
-                @error('creci')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+                <div class="mb-3 col-md-9">
+                    <input type="text" name="creci" placeholder="Digite seu Creci" value="{{$realtor->creci}}" class="form-control">
+                    @error('creci')
+                    <div class="mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <input type="text" name="name" placeholder="Digite seu nome" value="{{old('name')}}" value="{{$realtor->name}}">
-                @error('name')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+                <div class="col-12">
+                    <input type="text" name="name" placeholder="Digite seu nome" value="{{$realtor->name}}" class="form-control">
+                    @error('name')
+                    <div class="mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <button>Salvar</button>
+                <button class="btn btn-primary">Salvar</button>
             </form>
         </section>
     </main>
--- resources/views/realtors/index.blade.php
+++ resources/views/realtors/index.blade.php
@@ -4,65 +4,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>Cadastro de Corretores</title>
 </head>
 
 <body>
-    <main>
+    <main class="w-50 mt-3 container d-flex flex-column justify-content-center align-itens-center">
         <section>
-            <h1>Cadastro de Corretor</h1>
+            <h1 class="text-center">Cadastro de Corretor</h1>
             @isset($sucessMessage)
-            <div>
+            <div class="alert alert-success">
                 {{$sucessMessage}}
             </div>
             @endisset
-            <form action="{{route('realtors.store')}}" method="post">
+            <form action="{{route('realtors.store')}}" method="post" class="row g-3">
                 @csrf
 
-                <input type="text" name="cpf" placeholder="Digite seu CPF" value="{{old('cpf')}}">
-                @error('cpf')
-                <div>{{ $message }}</div>
-                @enderror
+                <div class="mb-3 col-md-3">
+                    <input type="text" name="cpf" placeholder="Digite seu CPF" value="{{old('cpf')}}" autofocus class="form-control">
+                    @error('cpf')
+                    <div class="mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <input type="text" name="creci" placeholder="Digite seu Creci" value="{{old('creci')}}">
-                @error('creci')
-                <div>{{ $message }}</div>
-                @enderror
+                <div class="mb-3 col-md-9">
+                    <input type="text" name="creci" placeholder="Digite seu Creci" value="{{old('creci')}}" class="form-control">
+                    @error('creci')
+                    <div class="mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <input type="text" name="name" placeholder="Digite seu nome" value="{{old('name')}}">
-                @error('name')
-                <div>{{ $message }}</div>
-                @enderror
+                <div class="col-12">
+                    <input type="text" name="name" placeholder="Digite seu nome" value="{{old('name')}}" class="form-control">
+                    @error('name')
+                    <div class="mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <button>Enviar</button>
+                <button class="btn btn-primary">Enviar</button>
             </form>
         </section>
 
         @if ($realtors->isNotEmpty())
-        <section>
-            <table>
+        <section class="mt-3">
+            <table class="table table-bordered">
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>CPF</th>
-                        <th>Creci</th>
-                        <th>Ações</th>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">CPF</th>
+                        <th scope="col">Creci</th>
+                        <th scope="col" class="text-center">Ações</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($realtors as $realtor)
                     <tr>
-                        <td>{{$realtor->id}}</td>
+                        <th scope="row">{{$realtor->id}}</th>
                         <td>{{$realtor->name}}</td>
                         <td>{{$realtor->cpf}}</td>
                         <td>{{$realtor->creci}}</td>
-                        <td>
+                        <td class="d-flex align-itens-center justify-content-center gap-3">
+                            <a href="{{route('realtors.edit', $realtor->id)}}">
+                                <span class="material-symbols-outlined">
+                                    edit
+                                </span>
+                            </a>
                             <form action="{{route('realtors.destroy', $realtor->id)}}" method="post">
                                 @csrf
                                 @method('delete')
-                                <a href="{{route('realtors.edit', $realtor->id)}}">Editar</a>
-                                <button>Excluir</button>
+                                <button class="btn btn-link p-0 border-0">
+                                    <span class="material-symbols-outlined">
+                                        delete
+                                    </span>
+                                </button>
                             </form>
                         </td>
                     </tr>
